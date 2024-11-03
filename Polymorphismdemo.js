8e366c53e62d9ed531674f0b102561fc1b7b6a7b
Permeshwary(Poonam) Bora <147245184+poonammirchandani123@users.noreply.github.com>
2024-11-03 17:34:02 +0000

    Add files via upload

---
+++ Polymorphismdemo.js
@@ -0,0 +1,30 @@
+class Shape
+{
+    draw()
+    {
+        return "I am genetic shape";
+    }
+}
+
+class Square extends Shape{
+    draw()
+    {
+        return "I am Square";
+    }
+}
+
+class Circle extends Shape{
+    draw()
+    {
+        return "I am Circle";
+    }
+}
+
+let s=new Shape();
+console.log(s.draw());
+
+s=new Square();
+console.log(s.draw());
+
+s=new Circle();
+console.log(s.draw());
